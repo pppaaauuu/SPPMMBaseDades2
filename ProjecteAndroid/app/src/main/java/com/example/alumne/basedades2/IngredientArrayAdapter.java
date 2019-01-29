9284dc9413bd9dbe65e0bcf1d69a82a942c1a53a
pppaaauuu <paupericas@iespaucasesnoves.cat>
2019-01-29 19:51:36 +0100

    Arreglar click listener

--- ProjecteAndroid/app/src/main/java/com/example/alumne/basedades2/IngredientArrayAdapter.java
+++ ProjecteAndroid/app/src/main/java/com/example/alumne/basedades2/IngredientArrayAdapter.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class IngredientArrayAdapter extends ArrayAdapter<Ingredient> implements View.OnClickListener{
+public class IngredientArrayAdapter extends ArrayAdapter<Ingredient>{
     private Context context;
     private List<Ingredient> ingredients;
     Ingredient ing;
@@ -22,6 +22,7 @@ public class IngredientArrayAdapter extends ArrayAdapter<Ingredient> implements 
     ImageView bas;
     ImageView queda;
     ImageView compra;
+    View view;
     //constructor
     public IngredientArrayAdapter(Context context, int resource, ArrayList<Ingredient> objects) {
         super(context, resource, objects);
@@ -29,11 +30,11 @@ public class IngredientArrayAdapter extends ArrayAdapter<Ingredient> implements 
         this.ingredients = objects;
     }
 
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, final ViewGroup parent) {
 
         ing = ingredients.get(position);
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-        final View view = inflater.inflate(R.layout.ingredient_a_llista, null);
+        view = inflater.inflate(R.layout.ingredient_a_llista, null);
         nom = (TextView) view.findViewById(R.id.textView);
         bas = (ImageView) view.findViewById(R.id.imageView);
         queda = (ImageView) view.findViewById(R.id.imageView2);
@@ -54,84 +55,84 @@ public class IngredientArrayAdapter extends ArrayAdapter<Ingredient> implements 
         }else{
             compra.setImageResource(android.R.drawable.presence_busy);
         }
-        bas.setOnClickListener(this);
-        /*bas.setOnClickListener(new android.widget.TextView.OnClickListener() {
+        bas.setOnClickListener(new android.widget.TextView.OnClickListener() {
             @Override
             public void onClick (View v) {
-
-                if(ing.isBasic()){
-                    ing.setBasic(false);
-                    bas.setImageResource(android.R.drawable.presence_busy);
-                }else{
-                    ing.setBasic(true);
-                    bas.setImageResource(android.R.drawable.presence_online);
-                }
-                DataSourceRebost dsr = new DataSourceRebost(context);
-                boolean upd = false;
-                try{
-                    dsr.open();
-                    upd = dsr.updateIng(ing);
-                    dsr.close();
-                }catch(SQLException e){
-                   // Toast.makeText(v.getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                }
-                if(!upd){
-                    Toast.makeText(v.getContext(), "No s'ha actualitzat ingredient", Toast.LENGTH_LONG).show();
-                }
-
-            }
-        } );*/
+                basicClick(position);
+            }});
         queda.setOnClickListener(new android.widget.TextView.OnClickListener() {
           @Override
           public void onClick (View v) {
-              if(ing.isQueda()){
-                  ing.setQueda(false);
-                  queda.setImageResource(android.R.drawable.presence_busy);
-              }else{
-                  ing.setQueda(true);
-                  queda.setImageResource(android.R.drawable.presence_online);
-              }
-
-          }
-        });
+              quedaClick(position);
+            }});
         compra.setOnClickListener(new android.widget.TextView.OnClickListener() {
             @Override
             public void onClick (View v) {
-                if(ing.isCompra()){
-                    ing.setCompra(false);
-                    compra.setImageResource(android.R.drawable.presence_busy);
-                }else{
-                    ing.setCompra(true);
-                    compra.setImageResource(android.R.drawable.presence_online);
-                }
-
-            }
-        });
+                compraClick(position);
+            }});
         return view;
     }
 
-    public void onClick(View v){
-      
-        if(v.getId() == R.id.imageView){
-            if(ing.isBasic()){
-                ing.setBasic(false);
-                bas.setImageResource(android.R.drawable.presence_busy);
-            }else{
-                ing.setBasic(true);
-                bas.setImageResource(android.R.drawable.presence_online);
-            }
-                DataSourceRebost dsr = new DataSourceRebost(context);
-                boolean upd = false;
-                try{
-                    dsr.open();
-                    upd = dsr.updateIng(ing);
-                    dsr.close();
-                }catch(SQLException e){
-                   // Toast.makeText(v.getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                }
-                if(!upd){
-                    Toast.makeText(v.getContext(), "No s'ha actualitzat ingredient", Toast.LENGTH_LONG).show();
-                }
+
+    public void basicClick(int pos){
+        ing = ingredients.get(pos);
+        if (ing.isBasic()) {
+            ing.setBasic(false);
+        } else {
+            ing.setBasic(true);
         }
+        DataSourceRebost dsr = new DataSourceRebost(context);
+        boolean upd = false;
+        try {
+            dsr.open();
+            upd = dsr.updateIng(ing);
+            dsr.close();
+        } catch (SQLException e) {}
+        if (!upd) {
+            Toast.makeText(context, "No s'ha actualitzat ingredient", Toast.LENGTH_LONG).show();
+        }
+        this.notifyDataSetChanged();
     }
+
+    public void quedaClick(int pos){
+        ing = ingredients.get(pos);
+        if (ing.isQueda()) {
+            ing.setQueda(false);
+        } else {
+            ing.setQueda(true);
+        }
+        DataSourceRebost dsr = new DataSourceRebost(context);
+        boolean upd = false;
+        try {
+            dsr.open();
+            upd = dsr.updateIng(ing);
+            dsr.close();
+        } catch (SQLException e) {}
+        if (!upd) {
+            Toast.makeText(context, "No s'ha actualitzat ingredient", Toast.LENGTH_LONG).show();
+        }
+        this.notifyDataSetChanged();
+    }
+
+    public void compraClick(int pos){
+        ing = ingredients.get(pos);
+        if (ing.isCompra()) {
+            ing.setCompra(false);
+        } else {
+            ing.setQueda(true);
+        }
+        DataSourceRebost dsr = new DataSourceRebost(context);
+        boolean upd = false;
+        try {
+            dsr.open();
+            upd = dsr.updateIng(ing);
+            dsr.close();
+        } catch (SQLException e) {}
+        if (!upd) {
+            Toast.makeText(context, "No s'ha actualitzat ingredient", Toast.LENGTH_LONG).show();
+        }
+        this.notifyDataSetChanged();
+    }
+
+
 }
